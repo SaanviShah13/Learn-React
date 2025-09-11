8880554c8b21b1c972257df78a8983c63208ab5b
SaanviShah13 <shahsaanvi13@gmail.com>
2025-09-12 00:48:45 +0530

    passing data onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import User from "./User";
 import College from "./College";
+import Student from "./Student";
 
 function App() {
   let userObject1={
@@ -22,16 +23,28 @@ function App() {
   }
 
   let collegeNames=['IIT','BITS','VIT','MIT','MUJ']
+
+  const [student, setStudent] = useState();
+
   return (
     <div>
       <>
         <h1>Props in React</h1>
         <br></br>
+        <h1>Passing props onClick</h1>
+        <hr></hr>
+        {student && <Student name={student}/>}
+        <button onClick={()=>setStudent("Saanvi")}>Update Student name</button>
+        <hr></hr>
+        <br></br>
+        <hr></hr>
+
         <h1>Passing Lists in props</h1>
         <hr></hr>
         <College names={collegeNames}/>
         <br></br>
         <hr></hr>
+        
         <h1>passing objects in props</h1>
         <User user={userObject1} />
         <User user={userObject2} />
---
+++ src/Student.jsx
@@ -0,0 +1,9 @@
+function Student({name}){
+    return(
+        <div>
+            <h2>Student name is {name}</h2>
+        </div>
+    )
+}
+
+export default Student
