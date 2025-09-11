68f77e39fdaa996e7c64eeef6827504950d10733
SaanviShah13 <shahsaanvi13@gmail.com>
2025-09-11 17:25:26 +0530

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Username from "./User"
 
 function App(){
   
-  const [display,setDisplay]=useState(true)
+  const [count, setCount]=useState(0)
 
   return(
     <>
-      <h1>Toggle button using usestate</h1>
-      
-      <button onClick={()=>setDisplay(!display)} >Show Name</button>
+      <h1>Conditional Rendering</h1>
+      <h1>count: {count}</h1>
+      <button onClick={()=>setCount(count +1 )} >counter button</button>
       {
-        display? <Username/>:null
+        count==0?<h2>Condition 0 </h2>
+        :count==1?<h2>Condition 1 </h2>
+        :count==2?<h2>Condition 2 </h2>
+        :count==3?<h2>Condition 3 </h2>
+        :count==4?<h2>Condition 4 </h2>
+        :<h2>Other condition</h2>
       }
     </>
   )
-
 }
-
 export default App
